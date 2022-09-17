49393100a8de28119e8713031dfe0148e019dee2
kiran205809 <kiranx.ananthram@intel.com>
2022-09-17 04:44:34 +0000

    First Commit

---
+++ src/firstprogrem/HelloWorld.java
@@ -0,0 +1,10 @@
+package firstprogrem;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello To Simplilearn World");
+	}
+
+}
